d0529b1a734b8c6f4116608d9a7bd6dcbd5bd0b9
sagecompscie <sagereid32@gmail.com>
2025-03-14 13:43:57 -0600

    temple album

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('ul');
+const list = document.querySelector('#list');
 const li = document.createElement('li');
 const delButton = document.createElement('button');
 
